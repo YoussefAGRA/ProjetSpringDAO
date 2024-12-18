8e8bbbdaee1e169bcd606f32d993733ba1a2e4cf
Youssef <125041634+Youssef-AGRA@users.noreply.github.com>
2024-12-18 16:45:55 +0100

    Update Project (CSS)

--- src/main/java/com/example/projet/ProjetApplication.java
+++ src/main/java/com/example/projet/ProjetApplication.java
@@ -2,12 +2,12 @@ package com.example.projet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ProjetApplication extends SpringbootServletInitial{
+public class ProjetApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProjetApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetApplication.class, args);
+    }
 }
