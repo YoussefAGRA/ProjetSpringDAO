67535ee655b8dfc79b6cc3eb69e7ea54ccbfcbc8
sabiir-ux <sabirkaddari036@gmail.com>
2024-12-18 13:26:17 +0100

    acceuill

--- src/main/java/com/example/projet/ProjetApplication.java
+++ src/main/java/com/example/projet/ProjetApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProjetApplication {
+public class ProjetApplication extends SpringbootServletInitial{
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetApplication.class, args);
